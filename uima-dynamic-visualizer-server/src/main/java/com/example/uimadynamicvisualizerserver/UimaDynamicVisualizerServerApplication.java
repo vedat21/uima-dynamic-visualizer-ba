c5d7cd4c884db9cddc7ce82a8f31048667223dde
Vedat Yildiz <49447893+vedat21@users.noreply.github.com>
2022-06-23 14:03:17 +0200

    Add files via upload
    init server

---
+++ uima-dynamic-visualizer-server/src/main/java/com/example/uimadynamicvisualizerserver/UimaDynamicVisualizerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.uimadynamicvisualizerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UimaDynamicVisualizerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UimaDynamicVisualizerServerApplication.class, args);
+	}
+
+}
---
+++ uima-dynamic-visualizer-server/src/test/java/com/example/uimadynamicvisualizerserver/UimaDynamicVisualizerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uimadynamicvisualizerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UimaDynamicVisualizerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
