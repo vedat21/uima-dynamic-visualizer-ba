49a1f704c794e12552ca53935f887b395a2f586d
yildiz <vedat-yildiz@hotmail.de>
2022-07-12 14:06:44 +0200

    update

--- server/src/main/java/root/UimaDynamicVisualizerServerApplication.java
+++ server/src/main/java/root/UimaDynamicVisualizerServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 // custom
 import root.database.UimaDocumentRepository;
-import root.entities.UimaDocument;
 
 
 @SpringBootApplication
