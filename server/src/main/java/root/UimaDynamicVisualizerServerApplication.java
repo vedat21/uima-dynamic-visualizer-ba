2f2e2045997071729c3af97d9402506577da1f9a
vyildiz <vedat.yildiz@iteratec.com>
2022-09-15 00:39:18 +0200

    extract uima documents from folder in env variable

--- server/src/main/java/root/UimaDynamicVisualizerServerApplication.java
+++ server/src/main/java/root/UimaDynamicVisualizerServerApplication.java
@@ -1,31 +1,43 @@
 package root;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.util.XmlCasDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-// custom
+// custom modules
 import root.database.UimaDocumentRepository;
+import root.entities.UimaDocument;
 
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class UimaDynamicVisualizerServerApplication implements CommandLineRunner {
 
-	@Autowired
-	UimaDocumentRepository repository;
+  @Autowired
+  UimaDocumentRepository repository;
 
-	public static void main(String... args) {
-		SpringApplication.run(UimaDynamicVisualizerServerApplication.class, args);
-	}
+  public static void main(String... args) {
+    SpringApplication.run(UimaDynamicVisualizerServerApplication.class, args);
+  }
 
-	@Override
-	public void run(String... args) throws Exception {
-//		repository.save(new UimaDocument());
-//		repository.save(new UimaDocument());
-//		repository.save(new UimaDocument("alles", "nochmalalles"));
+  @Override
+  public void run(String... args) throws Exception {
 
-	}
+    File folder = new File(System.getenv("path"));
+
+    for (File xmlDocument : folder.listFiles()) {
+      if (xmlDocument.getName().endsWith("xmi")) {
+        repository.save(new UimaDocument(xmlDocument));
+      }
+    }
+
+  }
 }
--- server/src/main/java/root/entities/UimaDocument.java
+++ server/src/main/java/root/entities/UimaDocument.java
@@ -3,29 +3,26 @@ package root.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import lombok.*;
 
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.util.XmlCasDeserializer;
 
-import java.io.InputStream;
-import java.io.FileInputStream;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
 import org.apache.uima.UIMAException;
+import org.apache.uima.util.XmlCasDeserializer;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.xml.sax.SAXException;
 import root.entities.sub.UimaEntitiy;
-import root.entities.sub.UimaType;
-
-import static java.util.Map.entry;
+import root.helper.DocumentTypes;
 
 
 /**
@@ -35,80 +32,91 @@ import static java.util.Map.entry;
 @Document(collection = "uimadocuments")
 public class UimaDocument {
 
-    @JsonIgnore
-    @Transient // ignoriere jcas in api und mongodb
-    private JCas jCas;
-    @Id
-    private String id;
+  @JsonIgnore
+  @Transient // ignoriere jcas in api und mongodb
+  private JCas jCas;
 
-    private List<UimaEntitiy> entities = new ArrayList<>();
-    private List<UimaEntitiy> pos = new ArrayList<>();
+  @Id
+  private String id;
+  private DocumentTypes documentTypes;
+  private Map<String, Object> types = new HashMap<>();
 
-    //zukunft
-    private Map<String, Object> types;
+  /**
+   * @throws UIMAException
+   * @throws IOException
+   * @throws SAXException
+   */
+  public UimaDocument(File xmlDocument) throws UIMAException, IOException, SAXException {
+    this.documentTypes = new DocumentTypes();
+    this.jCas = JCasFactory.createJCas();
 
+    //  deserialization of jcas from xmi. All information of document is stored in jcas.
+    try {
+      XmlCasDeserializer.deserialize(new FileInputStream(xmlDocument), this.jCas.getCas(), true);
 
-    /**
-     * @throws UIMAException
-     * @throws IOException
-     * @throws SAXException
-     */
-    public UimaDocument() throws UIMAException, IOException, SAXException {
-
-        //  deserialization of jcas from xmi. All information of document is stored in jcas.
-        this.jCas = JCasFactory.createJCas();
-        InputStream inputStream = null;
-        try {
-            // ändern
-            inputStream = new FileInputStream("C:\\Users\\vedat\\OneDrive\\Desktop\\Bachelorarbeit\\Bachelorarbeit_projekt\\uima-dynamic-visualizer-ba\\server\\src\\main\\resources\\data\\gercorpus-test.xmi");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        XmlCasDeserializer.deserialize(inputStream, this.jCas.getCas(), true);
+      // extract all information
+      this.extraxtNamedEntity();
+      this.extraxtPos();
 
 
-        // extract all information
-        this.extractTest();
-        this.extractPos();
-
-        this.types = Map.ofEntries(entry("pos", this.pos), entry("entities", this.entities));
-
-
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
 
+  }
 
-    /**
-     * Noch am Testen. Soll für generic Type möglich sein. (Nicht nur NamedEntity)
-     */
 
-    public void extractTest() {
-        for (NamedEntity namedEntity : JCasUtil.select(this.jCas, NamedEntity.class)) {
+  /**
+   * Noch am Testen. Soll für generic Type möglich sein.
+   */
+/*
+    public void extractAllTypes(Jcas jcas) {
+        for (Class<T> type : this.documentTypes.getAllTypes()){
+            for (type.getClass() namedEntity : JCasUtil.select(this.jCas, type)) {
 
-            UimaEntitiy uimaEntitiy = new UimaEntitiy(
+                UimaEntitiy uimaEntitiy = new UimaEntitiy(
                     namedEntity.getValue(),
                     namedEntity.getCoveredText(),
                     namedEntity.getBegin(),
                     namedEntity.getEnd()
 
-            );
+                );
 
-            this.entities.add(uimaEntitiy);
+                this.entities.add(uimaEntitiy);
+            }
         }
     }
+ */
+  public void extraxtNamedEntity() {
+    List<UimaEntitiy> entities = new ArrayList<>();
+    for (NamedEntity namedEntity : JCasUtil.select(this.jCas, NamedEntity.class)) {
 
+      UimaEntitiy uimaEntitiy = new UimaEntitiy(
+          namedEntity.getValue(),
+          namedEntity.getCoveredText(),
+          namedEntity.getBegin(),
+          namedEntity.getEnd()
+      );
 
-    public void extractPos() {
-        for (Token token : JCasUtil.select(this.jCas, Token.class)) {
-
-            UimaEntitiy uimaEntitiy = new UimaEntitiy(
-                    token.getPosValue(),
-                    token.getCoveredText(),
-                    token.getBegin(),
-                    token.getEnd()
-            );
-
-            this.pos.add(uimaEntitiy);
-        }
+      entities.add(uimaEntitiy);
     }
+
+    types.put("entities", entities);
+  }
+
+  public void extraxtPos() {
+    List<UimaEntitiy> pos = new ArrayList<>();
+    for (Token token : JCasUtil.select(this.jCas, Token.class)) {
+      UimaEntitiy uimaEntitiy = new UimaEntitiy(
+          token.getPosValue(),
+          token.getCoveredText(),
+          token.getBegin(),
+          token.getEnd()
+      );
+
+      pos.add(uimaEntitiy);
+    }
+    types.put("pos", pos);
+  }
 
 }
