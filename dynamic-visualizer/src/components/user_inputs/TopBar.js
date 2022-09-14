7b4de74b7f1c578c08c59511bcac9257d7d49267
vyildiz <vedat.yildiz@iteratec.com>
2022-09-15 00:33:14 +0200

    add tooltip and refactor

--- dynamic-visualizer/src/components/user_inputs/TopBar.js
+++ dynamic-visualizer/src/components/user_inputs/TopBar.js
@@ -5,28 +5,27 @@ import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Slide from '@mui/material/Slide';
-import {Button, IconButton, TextField} from "@mui/material";
+import {Button, IconButton, TextField, Tooltip} from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 
 // custom modules
 import SelectContainer from "./SelectContainer";
 import {usedColors} from "../../helper/envConst";
 
-
 /**
  * Quelle: https://mui.com/material-ui/react-app-bar/
  */
 function HideOnScroll(props) {
-    const {children, window} = props;
-    const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
-    });
+  const {children, window} = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
 
-    return (
-        <Slide appear={false} direction="down" in={!trigger}>
-            {children}
-        </Slide>
-    );
+  return (
+      <Slide appear={false} direction="down" in={!trigger}>
+        {children}
+      </Slide>
+  );
 }
 
 /**
@@ -37,53 +36,66 @@ function HideOnScroll(props) {
  * Quelle: https://mui.com/material-ui/react-app-bar/
  */
 function TopBar(props) {
-    return (
-        <React.Fragment>
-            <CssBaseline/>
-            <HideOnScroll {...props}>
-                <AppBar sx={{backgroundColor: usedColors.primary}}>
-                    <Toolbar>
+  return (
+      <React.Fragment>
+        <CssBaseline/>
+        <HideOnScroll {...props}>
+          <AppBar sx={{backgroundColor: usedColors.primary}}>
+            <Toolbar>
 
-                        {/* if editable render typohraphy else render textfield to edit title  */}
-                        {!props.editable &&
-                            <Typography variant="h5" component="div" sx={{flexGrow: 1}}> {/* buttons on the right */}
-                                {props.title}
-                            </Typography>
-                        }
-                        {props.editable &&
-                            <TextField
-                                defaultValue={props.title}
-                                onKeyDown={props.editTitle}
-                                sx={{flexGrow: 1, input: {color: usedColors.secondary}}}></TextField>
-                        }
+              {/* if editable render typohraphy else render textfield to edit title  */}
+              {!props.editable &&
+                  <Typography
+                      variant="h5"
+                      component="div"
+                      sx={{flexGrow: 1}}
+                  >
+                    {props.title}
+                  </Typography>
+              }
+              {props.editable &&
+                  <Tooltip title={'Press enter to save Title'}>
+                    <TextField
+                        defaultValue={props.title}
+                        onKeyDown={props.editTitle}
+                        sx={{
+                          flexGrow: 1,
+                          input: {color: usedColors.secondary}
+                        }}
+                    />
+                  </Tooltip>
+              }
 
-                        {/* button to enable/disable editor modus. Only renders if topbar has prop editable */}
-                        {props.editable != null &&
-                            <Button
-                                color={props.editable ? "inherit" : "inherit"}
-                                variant={props.editable ? "contained" : "text"}
-                                onClick={props.onEditableClicked}
-                            >
-                                EDIT
-                            </Button>
-                        }
+              {/* button to enable/disable editor modus. Only renders if topbar has prop editable */}
+              {props.editable != null &&
+                  <Tooltip title={'Also saves the current Presentation'}>
+                    <Button
+                        color={props.editable ? "inherit" : "inherit"}
+                        variant={props.editable ? "contained" : "text"}
+                        onClick={props.onEditableClicked}
+                    >
+                      EDIT
+                    </Button>
+                  </Tooltip>
+              }
 
-                        {/* button to add a new presentation. Only renders if topbar has no prop editable */}
-                        {props.editable == null &&
-                            <Button
-                                onClick={props.addPresentation}
-                                color="inherit"
-                            > Add Presentation</Button>
-                        }
-                    </Toolbar>
-                    {/* input area for creating new visualization */}
-                    {props.editable && <SelectContainer addVisualization={props.addVisualization}
-                    />}
-                </AppBar>
-            </HideOnScroll>
-            <Toolbar/> {/* Damit Header unteren content nicht überdeckt */}
-        </React.Fragment>
-    );
+              {/* button to add a new presentation. Only renders if topbar has no prop editable */}
+              {props.editable == null &&
+                  <Button
+                      onClick={props.addPresentation}
+                      color="inherit"
+                  > Add Presentation</Button>
+              }
+            </Toolbar>
+            {/* input area for creating new visualization */}
+            {props.editable && <SelectContainer
+                addVisualization={props.addVisualization}
+            />}
+          </AppBar>
+        </HideOnScroll>
+        <Toolbar/> {/* Damit Header unteren content nicht überdeckt */}
+      </React.Fragment>
+  );
 }
 
 export default TopBar;
--- dynamic-visualizer/src/components/visualizations/topbars/ChartTopBar.js
+++ dynamic-visualizer/src/components/visualizations/topbars/ChartTopBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {IconButton, TextField} from "@mui/material";
+import {IconButton, TextField, Tooltip} from "@mui/material";
 import {DeleteForever, Edit} from "@mui/icons-material";
 import {v4 as uuid} from 'uuid';
 
@@ -11,48 +11,57 @@ import {v4 as uuid} from 'uuid';
  * @constructor
  */
 function ChartTopBar(props) {
-    return (
-        <>
-            {
-                props.editable && // only show topbar when editor modus is enabled
-                <>
-                    <IconButton onClick={props.onDeleteComponentClicked}>
-                        <DeleteForever/>
-                    </IconButton>
-                    <IconButton onClick={props.reverseEditLabel}>
-                        <Edit/>
-                    </IconButton>
-                    {/* displays textfield for setting label or for setting minimal occurence of data */}
-                    {props.editLabel &&
-                        <TextField
-                            key={uuid()}
-                            size="small"
-                            label="Label"
-                            id="outlined-basic"
-                            variant="outlined"
-                            autoComplete="off"
-                            defaultValue={props.label}
-                            onKeyDown={props.changeLabel}
-                        />
-                    }
-                    {!props.editLabel &&
-                        <TextField
-                            key={uuid()}
-                            id="outlined-basic"
-                            variant="outlined"
-                            size="small"
-                            label="Limit"
-                            autoComplete="off"
-                            type="number"
-                            min="0"
-                            onKeyDown={props.changeLimit}
-                            defaultValue={props.limit}
-                        />
-                    }
-                </>
-            }
-        </>
-    );
+  return (
+      <>
+        {
+            props.editable && // only show topbar when editor modus is enabled
+            <>
+              <Tooltip title={"Delete visualization"}>
+                <IconButton onClick={props.onDeleteComponentClicked}>
+                  <DeleteForever/>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title={"Click for editing title/limit"}>
+                <IconButton onClick={props.reverseEditLabel}>
+                  <Edit/>
+                </IconButton>
+              </Tooltip>
+              {/* displays textfield for setting label or for setting minimal occurence of data */}
+              {props.editLabel &&
+                  <Tooltip title={"Press enter to save title"}>
+                    <TextField
+                        key={uuid()}
+                        size="small"
+                        label="Label"
+                        id="outlined-basic"
+                        variant="outlined"
+                        autoComplete="off"
+                        defaultValue={props.label}
+                        onKeyDown={props.changeLabel}
+                    />
+                  </Tooltip>
+              }
+              {!props.editLabel &&
+                  <Tooltip title={"Press enter to submit filter"}>
+                    <TextField
+                        key={uuid()}
+                        id="outlined-basic"
+                        variant="outlined"
+                        size="small"
+                        label="Limit"
+                        autoComplete="off"
+                        type="number"
+                        min="0"
+                        onKeyDown={props.changeLimit}
+                        defaultValue={props.limit}
+                    />
+                  </Tooltip>
+
+              }
+            </>
+        }
+      </>
+  );
 }
 
 export default ChartTopBar;
